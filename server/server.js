850ba0cc58d6a5d949566bce501158be48eddc2f
Udit Kumar Sharma <uditkumarsharma@Udits-MacBook-Air.local>
2017-10-23 18:38:56 +0530

    add sockets connect and disconnect

--- server/server.js
+++ server/server.js
@@ -1,13 +1,23 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 const publicPath=path.join(__dirname,'../public')
 
 
 var app = express();
+var server=http.createServer(app)
+var io =socketIO(server);
 app.use(express.static(publicPath));
 
+io.on('connection',(socket)=>{
+    console.log('New User connected');
 
+    socket.on('disconnect',()=>{
+       console.log('user disconnected');
+    });
+});
 
-app.listen(3000,()=>{
+server.listen(3000,()=>{
    console.log('Server is listing 3000 port');
 });
